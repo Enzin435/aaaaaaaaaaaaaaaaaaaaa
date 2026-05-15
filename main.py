ab99a5f3470c3bb7c627ecfd1ff125ec1825c23e
Enzin435 <enzosmarchelli2013@gmail.com>
2026-05-15 17:46:24 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+meme_dict = {
+    "VLW": "Abreviação de valeu",
+    "HATER": "pessoa que constantemente critica os outros",
+    }
+
+word = input("Digite uma palavra moderna que vc n entenda(ESCREVA EM LETRAS MAIÚSCuLAS")
+
+if word in meme_dict_keys():
+    print("Vou pesquisar um minuto")
+
+elif word == "HATER":
+    print("Pessoa que constantemente crítica os outros")
+
+elif word == "VLW":
+    print("Abreviação de valeu")
+
+else:
+    print("Não o siginificado desculpa")
